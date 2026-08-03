bf821982a43d84189bfd4adcbeff48a93fcaecf8
Prince Kumar <princekumar9661176@gmail.com>
2026-08-03 19:45:42 +0530

    add new feature

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    let phone = "918292388768"; 
+
+    let text = `Hello Prince,
+Name: ${name}
+Email: ${email}
+
+Message:
+${message}`;
+
+    let url = `https://wa.me/${phone}?text=${encodeURIComponent(text)}`;
+
+    window.open(url, "_blank");
+});
